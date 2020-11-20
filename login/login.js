1eae9054d5c7f60091714b2076b857b648fd41b8
yyqx <yyqx@163.com>
2020-11-20 11:19:54 +0800

    BE 注册功能 1

--- login/login.js
+++ login/login.js
@@ -6,3 +6,33 @@ $("#goto-login").on("click", function () {
   $("#login").show();
   $("#register").hide();
 });
+// -----------------------------------------------------------注册
+$("#register .layui-form").on("submit", function (e) {
+  // 1.阻止默认行为；
+  e.preventDefault();
+
+  // 2.收集数据
+  var data = $(this).serialize();
+
+  // 3.接口，发出请求
+  // 发生数据
+  $.ajax({
+    type: "POST",
+    url: "http://ajax.frontend.itheima.net/api/reguser",
+    data: data,
+    success: function (res) {
+      // 弹窗：msg 简单弹窗、会自动消失；
+      layer.msg(res.message);
+
+      // 业务设计：
+      if (res.status == 0) {
+        $("#register").hide();
+        $("#login").show();
+
+        // 需求：原生方式；重置！
+        //       注意慎重使用this;
+        $("#register .layui-form")[0].reset();
+      }
+    }
+  });
+});
